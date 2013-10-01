ed01cc6a8b7e04c31062af141b30a3b0893b95d4
Dirk Theisen <d.theisen@objectpark.net>
2013-10-01 22:32:59 +0200

    Fixed repl server.

--- S-Explorer/chibi-repl-server.scm
+++ S-Explorer/chibi-repl-server.scm
@@ -2,20 +2,26 @@
 ;; Simple R7RS repl server, using (srfi 18) threads and the
 ;; run-net-server utility from (chibi net server).
 
-(import (scheme base) (scheme write) (srfi 18) (srfi 38) (chibi net server) (scheme eval))
+(import (scheme base)
+    (scheme write) 
+    (srfi 18) 
+    (srfi 38) 
+    (scheme repl)
+    (chibi net server) 
+    (scheme eval))
 
-;; Copy each input line to output.
+;; evaluate all input from line-port and write the result to output port out.
 (define (repl-handler in out sock addr)
   (let ((line (read-line in)))
     (cond
      ((not (or (eof-object? line) (equal? line "")))
       (display "read: ") (write line) (newline)
       (let* (
-        (line-port (open-input-string line))
-        (result (eval (read/ss line-port) (scheme-report-environment 7))))
-        (write/ss result out))
-      (newline out)
-      (flush-output-port out)))))
+          (line-port (open-input-string line))
+          (result (eval (read/ss line-port) (interaction-environment))))
+        (write/ss result out)
+        (newline out)
+        (flush-output-port out))))))
 
 ;; Start the server on localhost:5556 dispatching clients to repl-handler.
 (run-net-server 5556 repl-handler)
