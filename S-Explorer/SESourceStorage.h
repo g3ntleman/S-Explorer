0ade7d5942fbf6ce91a4e3eaa549344d9e2b2aa0
Dirk Theisen <d.theisen@objectpark.net>
2016-05-05 02:57:16 +0200

    Improved word selection on double click.

---
+++ S-Explorer/SESourceStorage.h
@@ -0,0 +1,13 @@
+//
+//  SESourceStorage.h
+//  S-Explorer
+//
+//  Created by Dirk Theisen on 05.05.16.
+//  Copyright © 2016 Cocoanuts. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SESourceStorage : NSTextStorage
+
+@end
---
+++ S-Explorer/SESourceStorage.m
@@ -0,0 +1,13 @@
+//
+//  SESourceStorage.m
+//  S-Explorer
+//
+//  Created by Dirk Theisen on 05.05.16.
+//  Copyright © 2016 Cocoanuts. All rights reserved.
+//
+
+#import "SESourceStorage.h"
+
+@implementation SESourceStorage
+
+@end
